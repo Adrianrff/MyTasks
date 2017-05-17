9fd14be0557cfd6d60651d756a89157063469d33
Adrianrff <adrianrff@gmail.com>
2017-05-17 13:46:35 -0400

    Normal

--- app/src/main/java/com/adrapps/mytasks/views/NewTaskOrEditActivity.java
+++ app/src/main/java/com/adrapps/mytasks/views/NewTaskOrEditActivity.java
@@ -345,7 +345,6 @@ public class NewTaskOrEditActivity extends AppCompatActivity
                         notificationTV.setText(DateHelper.timeInMillsToTimeOnly(reminderCalendar.getTimeInMillis()));
                         clearReminder.setVisibility(View.VISIBLE);
                         notifLayout.setVisibility(View.VISIBLE);
-
                         setRepeatSpinnerAdapter(reminderCalendar.getTimeInMillis(), true);
                         repeatSpinner.setSelection(Co.REMINDER_DAILY);
                         dialog.dismiss();
